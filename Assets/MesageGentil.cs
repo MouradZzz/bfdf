114c1f9352ffd8c595bcf43ff3aa9b218694c34c
El Ghali <mouradelghali84@gmail.com>
2019-09-12 15:58:24 +0200

    AddScriptsInstanceSphere

--- Assets/MesageGentil.cs
+++ Assets/MesageGentil.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MesageGentil : MonoBehaviour
 {
+    public GameObject r;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(r, Vector3.one, Quaternion.identity);
     }
 
     // Update is called once per frame
